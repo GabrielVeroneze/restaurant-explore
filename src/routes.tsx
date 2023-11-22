a37de6101104d256150e54f959b60b189bbe6800
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-21 21:18:25 -0300

    :sparkles: feat: Cria arquivo de rotas para a aplicação

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={} />
+            <Route path="/restaurantes" element={} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
