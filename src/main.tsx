b0e44963bfcb02c349f0fa9a643b82a030b1cfd7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-22 22:35:10 -0300

    :sparkles: feat: Adiciona arquivos de reset e estilização global

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@/sass/_reset.scss'
+import '@/sass/_global.scss'
 import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
