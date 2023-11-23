b0159b624c431e8708027ad6fef624eb13055f38
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-23 15:22:47 -0300

    :sparkles: feat: Cria página de vitrine para os restaurantes

---
+++ src/pages/VitrineRestaurantes/index.tsx
@@ -0,0 +1,17 @@
+import Banner from '@/components/Banner'
+import NavBar from '@/components/NavBar'
+import Restaurantes from '@/components/Restaurantes'
+import Rodape from '@/components/Rodape'
+
+const VitrineRestaurantes = () => {
+    return (
+        <>
+            <NavBar />
+            <Banner />
+            <Restaurantes />
+            <Rodape />
+        </>
+    )
+}
+
+export default VitrineRestaurantes
