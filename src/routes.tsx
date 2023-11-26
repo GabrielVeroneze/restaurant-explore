2d599b67801d8048bb1e37f72bd749b0a7418d8a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-25 22:58:08 -0300

    :sparkles: feat: Cria rota para página de administração de restaurantes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'
 import VitrineRestaurantes from '@/pages/VitrineRestaurantes'
+import AdministracaoRestaurantes from '@/pages/Administracao/Restaurantes/AdministracaoRestaurantes'
 
 const AppRoutes = () => {
     return (
@@ -8,6 +9,9 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+                <Route path="/admin/restaurantes">
+                    <Route index element={<AdministracaoRestaurantes />} />
+                </Route>
             </Routes>
         </Router>
     )
