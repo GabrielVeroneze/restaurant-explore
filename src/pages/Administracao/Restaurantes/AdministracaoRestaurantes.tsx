b080488a522b3cffcd165bbd5efe64970f5c8725
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-25 23:04:56 -0300

    :sparkles: feat: Faz requisição e manipula dados da API
    Faz requisição para a API com os dados dos restaurantes

--- src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/pages/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,17 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { IRestaurante } from '@/interfaces/IRestaurante'
 
 const AdministracaoRestaurantes = () => {
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(resposta => {
+                setRestaurantes(resposta.data)
+            })
+    }, [])
 
     return (
         <TableContainer component={Paper}>
