bc552130b016e53e72af0e4778dacee0337ef212
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-25 23:17:47 -0300

    :sparkles: feat: Submete o novo restaurante para API
    Faz requisição "POST" para a API enviando um novo restaurante, para a lista de restaurantes.

--- src/pages/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/pages/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -5,6 +5,18 @@ import { Button, TextField } from '@mui/material'
 const FormularioRestaurante = () => {
     const [nomeRestaurante, setNomeRestaurante] = useState<string>('')
 
+    const handleSubmit = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+
+        axios
+            .post('http://localhost:8000/api/v2/restaurantes/', {
+                nome: nomeRestaurante,
+            })
+            .then(() => {
+                alert('Restaurante cadastrado com sucesso!')
+            })
+    }
+
     return (
         <form onSubmit={evento => handleSubmit(evento)}>
             <TextField
