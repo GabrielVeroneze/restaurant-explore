edc99bb2543aca2f3e67018b2a98c47557205159
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-27 18:21:31 -0300

    :sparkles: feat: Cria função para carregar restaurantes

--- src/hooks/useAdministrarRestaurantes.ts
+++ src/hooks/useAdministrarRestaurantes.ts
@@ -6,26 +6,31 @@ export const useAdministrarRestaurantes = () => {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
 
     useEffect(() => {
+        carregarRestaurantes()
+    }, [])
+
+    const carregarRestaurantes = () => {
         axios
             .get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
             .then(resposta => {
                 setRestaurantes(resposta.data)
             })
-    }, [])
+    }
 
     const excluirRestaurante = (restauranteId: number) => {
-        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteId}/`)
-        .then(() => {
-            setRestaurantes(
-                restaurantes.filter(
-                    restaurante => restaurante.id !== restauranteId
+        axios
+            .delete(`http://localhost:8000/api/v2/restaurantes/${restauranteId}/`)
+            .then(() => {
+                setRestaurantes(
+                    restaurantes.filter(
+                        restaurante => restaurante.id !== restauranteId
+                    )
                 )
-            )
-        })
+            })
     }
 
     return {
         restaurantes,
-        excluirRestaurante
+        excluirRestaurante,
     }
 }
