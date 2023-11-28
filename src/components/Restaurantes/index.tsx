dd207d26d19ef55f2038c34a610871fbd9dbd96e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-28 18:44:40 -0300

    :sparkles: feat: Adiciona campo para pesquisa e botão para pesquisar

--- src/components/Restaurantes/index.tsx
+++ src/components/Restaurantes/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import { IconButton, TextField } from '@mui/material'
 import { useBuscarRestaurantes } from '@/hooks/useBuscarRestaurantes'
+import SearchIcon from '@mui/icons-material/Search';
 import Restaurante from './Restaurante'
 import styles from './Restaurantes.module.scss'
 
@@ -7,9 +10,21 @@ const Restaurantes = () => {
 
     return (
         <section className={styles.restaurantes}>
-            <h1 className={styles.titulo}>
-                Os restaurantes mais <em>bacanas</em>!
-            </h1>
+            <div className={styles.container}>
+                <h1 className={styles.titulo}>
+                    Os restaurantes mais <em>bacanas</em>!
+                </h1>
+                <form onSubmit={evento => handleSearchSubmit(evento)}>
+                    <TextField
+                        label="Busque por Restaurantes"
+                        variant="outlined"
+                        size="small"
+                    />
+                    <IconButton type="submit" aria-label="busca">
+                        <SearchIcon />
+                    </IconButton>
+                </form>
+            </div>
             {listaRestaurantes?.map(item => (
                 <Restaurante key={item.id} restaurante={item} />
             ))}
