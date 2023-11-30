b0247f36ff9bff9b90a4b8015fe90e1d57ffe982
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-29 23:14:52 -0300

    :sparkles: feat: Cria layout para páginas de administração
    Utiliza componentes do MUI para criar um layout para todas as páginas da relacionadas a administração.

---
+++ src/components/LayoutAdmin/index.tsx
@@ -0,0 +1,43 @@
+import { AppBar, Box, Button, Container, Link, Paper, Toolbar, Typography } from '@mui/material'
+import { Outlet, Link as RouterLink } from 'react-router-dom'
+
+const LayoutAdmin = () => {
+    return (
+        <>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <Typography variant="h6">Administração</Typography>
+                        <Box sx={{ width: '100%' }}>
+                            <Link
+                                component={RouterLink}
+                                to="/admin/restaurantes"
+                            >
+                                <Button sx={{ color: 'white', my: 2 }}>
+                                    Restaurantes
+                                </Button>
+                            </Link>
+                            <Link
+                                component={RouterLink}
+                                to="/admin/restaurantes/novo"
+                            >
+                                <Button sx={{ color: 'white', my: 2 }}>
+                                    Novo Restaurante
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Box>
+                <Container maxWidth="lg" sx={{ mt: 4 }}>
+                    <Paper elevation={2} sx={{ p: 2 }}>
+                        <Outlet />
+                    </Paper>
+                </Container>
+            </Box>
+        </>
+    )
+}
+
+export default LayoutAdmin
