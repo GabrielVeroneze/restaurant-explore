3f3377410b1a2696283f489984fa0517923e960f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-30 22:17:56 -0300

    :sparkles: feat: Cria rota para página com formulário dos pratos

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import VitrineRestaurantes from '@/pages/VitrineRestaurantes'
 import AdministracaoRestaurantes from '@/pages/Administracao/Restaurantes/AdministracaoRestaurantes'
 import FormularioRestaurante from '@/pages/Administracao/Restaurantes/FormularioRestaurante'
 import AdministracaoPratos from '@/pages/Administracao/Pratos/AdministracaoPratos'
+import FormularioPrato from '@/pages/Administracao/Pratos/FormularioPrato'
 import LayoutAdmin from '@/components/LayoutAdmin'
 
 const AppRoutes = () => {
@@ -17,6 +18,7 @@ const AppRoutes = () => {
                     <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
                     <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
                     <Route path="pratos" element={<AdministracaoPratos />} />
+                    <Route path="pratos/novo" element={<FormularioPrato />} />
                 </Route>
             </Routes>
         </Router>
